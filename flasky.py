970785d3ff4cef8ad8ee9cbf2201f78cc7d6f116
Vibha Ravindra <vibhashree.ravindra@sjsu.edu>
2020-05-02 18:31:47 -0700

    Initial parsing of request & input validation done

--- flasky.py
+++ flasky.py
@@ -15,7 +15,34 @@ def mainpage():
 
 @app.route("/dashboard", methods=["GET"])
 def investment_calculator():
-    
+    total_amount = request.args.get('search', default = 0, type = int)
+    ethical_on = request.args.get('EthicalInvest', default = "off", type = str)
+    growth_on = request.args.get('GrowthInvest', default = "off", type = str)
+    index_on = request.args.get('IndexInvest', default = "off", type = str)
+    quality_on = request.args.get('QualityInvest', default = "off", type = str)
+    value_on = request.args.get('ValueInvest', default = "off", type = str)
+
+    strategies_selected = []
+    if "on" == ethical_on:
+        strategies_selected.append("Ethical Investing")
+    if "on" == growth_on:
+        strategies_selected.append("Growth Investing")
+    if "on" == index_on:
+        strategies_selected.append("Index Investing")
+    if "on" == quality_on:
+        strategies_selected.append("Quality Investing")
+    if "on" == value_on:
+        strategies_selected.append("Value Investing")
+
+    if total_amount < 5000 or len(strategies_selected) > 2 or len(strategies_selected) == 0:
+        return "Invalid data passed. Total amount should be atleast 5000 and should select one or two strategies only"
+
+
+    ethical_stock_symbol_array = ['AAPL', 'MSFT', 'ADBE']
+        growth_stock_symbol_array = ['FIT', 'GPRO', 'NVDA']
+        index_stock_symbol_array = ['FB', 'AMZN', 'HMC']
+        quality_stock_symbol_array = ['JPM', 'WMT', 'BBY']
+        value_stock_symbol_array = ['TSLA', 'TWTR', 'GOOG']
     return render_template("themes/monster-admin/lite version/pages-dashboard.html")
 
 
