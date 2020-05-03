4bf1a4af65c4c116caa1265f1f23190af56adfe1
Vibha Ravindra <vibhashree.ravindra@sjsu.edu>
2020-05-02 18:57:05 -0700

    main method to handle input.

--- flasky.py
+++ flasky.py
@@ -4,6 +4,7 @@ import datetime
 from iexfinance.stocks import Stock
 import iexfinance.utils.exceptions as excep
 from pytz import timezone
+import traceback
 
 app = Flask(__name__)
 
@@ -16,33 +17,60 @@ def mainpage():
 @app.route("/dashboard", methods=["GET"])
 def investment_calculator():
     total_amount = request.args.get('search', default = 0, type = int)
-    ethical_on = request.args.get('EthicalInvest', default = "off", type = str)
-    growth_on = request.args.get('GrowthInvest', default = "off", type = str)
-    index_on = request.args.get('IndexInvest', default = "off", type = str)
-    quality_on = request.args.get('QualityInvest', default = "off", type = str)
-    value_on = request.args.get('ValueInvest', default = "off", type = str)
+
+    request_strategy_mapping = {
+        "EthicalInvest": "Ethical Investing",
+        "GrowthInvest": "Growth Investing",
+        "IndexInvest": "Index Investing",
+        "QualityInvest": "Quality Investing",
+        "ValueInvest": "Value Investing",
+    }
+
+    stock_mapping = {
+        "Ethical Investing" : ['AAPL', 'ADBE', 'NSRGY'],
+        "Growth Investing" : ['GDDY', 'ISRG', 'MTH'],
+        "Index Investing" : ['VTI', 'IXUS', 'ILTB'],
+        "Quality Investing" : ['UFPI', 'VIPS', 'AB'],
+        "Value Investing" : ['NRG', 'VIAC', 'NCLH']
+    }
 
     strategies_selected = []
-    if "on" == ethical_on:
-        strategies_selected.append("Ethical Investing")
-    if "on" == growth_on:
-        strategies_selected.append("Growth Investing")
-    if "on" == index_on:
-        strategies_selected.append("Index Investing")
-    if "on" == quality_on:
-        strategies_selected.append("Quality Investing")
-    if "on" == value_on:
-        strategies_selected.append("Value Investing")
+    for k,v in request_strategy_mapping.items():
+        if "on" == request.args.get(k, default = "off", type = str):
+            strategies_selected.append(v)
 
     if total_amount < 5000 or len(strategies_selected) > 2 or len(strategies_selected) == 0:
         return "Invalid data passed. Total amount should be atleast 5000 and should select one or two strategies only"
 
+    print ("Amount input: {}".format(total_amount))
+    print ("Strategies Selected: {}".format(strategies_selected))
 
-    ethical_stock_symbol_array = ['AAPL', 'MSFT', 'ADBE']
-        growth_stock_symbol_array = ['FIT', 'GPRO', 'NVDA']
-        index_stock_symbol_array = ['FB', 'AMZN', 'HMC']
-        quality_stock_symbol_array = ['JPM', 'WMT', 'BBY']
-        value_stock_symbol_array = ['TSLA', 'TWTR', 'GOOG']
+    try:
+        investment_results = []
+        investment_result_details = []
+
+        amount_per_strategy = total_amount * 1.0 / len(strategies_selected)
+        for strategy in strategies_selected:
+            res, res_details = get_investment_results(strategy, amount_per_strategy, stock_mapping[strategy])
+            investment_results.append([strategy, res])
+            investment_result_details.append([strategy, res_details])
+
+        print("Invest results: {}".format(investment_results))
+        print("Invest result Details: {}".format(investment_result_details))
+
+        if len(strategies_selected) == 1 and len(investment_results) == 1:
+            return render_template("themes/monster-admin/lite version/pages-dashboard.html", results = investment_results, details = investment_result_details)
+        elif len(strategies_selected) == 2 and len(investment_results) == 2:
+            return render_template("themes/monster-admin/lite version/pages-dashboard.html", results = investment_results, details = investment_result_details)
+        else:
+            return "Internal server error"
+
+    except ValueError:
+        print('Invalid stock symbol configured')
+    except Exception as e:
+        print('Error connecting to data source, please try again.')
+        print(traceback.format_exc())
+
     return render_template("themes/monster-admin/lite version/pages-dashboard.html")
 
 
@@ -50,5 +78,6 @@ def get_investment_results(strategy, amount_per_strategy, stocks_array):
     invest_results = []
     invest_results_detailed = []
     return invest_results, invest_results_detailed
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
